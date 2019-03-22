14b54e3b7c2015d101a4ec49ecc3b06fa40634a3
Jackthehaxk21 <gangnam253@gmail.com>
2019-03-22 20:21:44 +0000

    MakeFile, used

--- test.cmd
+++ test.cmd
@@ -1,23 +1,15 @@
 @echo off
 
-echo --- Cleaning tmp ---
-rm game.o
-rm game.elf
-rm game.gba
-rm dist/game.gba
-echo --- Building ROM ---
-arm-none-eabi-gcc -c src/spawn.c -mthumb-interwork -mthumb -O2 -o spawn.o
-arm-none-eabi-gcc -c src/saveManager.c -mthumb-interwork -mthumb -O2 -o saveManager.o
-arm-none-eabi-gcc saveManager.o spawn.o -mthumb-interwork -mthumb -specs=gba.specs -o game.elf
-arm-none-eabi-objcopy -v -O binary game.elf game.gba
-rm game.o
-rm game.elf
-mkdir dist
-MOVE game.gba dist/
+echo --- Cleaning old env ---
+rm -rf build
+rm Game.gba
+rm Game.elf
+echo --- Running Make ---
+make
 echo --------------------
 
 echo Started VisualBoyAdvance
 
-"C:\Users\Jack\Desktop\GameBoy Development\GBA Emulator (VBA)\VisualBoyAdvance.exe" dist/game.gba
+"C:\Users\Jack\Desktop\GameBoy Development\GBA Emulator (VBA)\VisualBoyAdvance.exe" Game.gba
  
 echo VisualBoyAdvance Closed
